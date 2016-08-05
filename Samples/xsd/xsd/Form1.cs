3d0455554dbed9fdca40d8b0c1f9feaf454da50f
lwFace <zsddn2003@outlook.com>
2016-08-05 14:14:14 +0800

    增加xsd例程
    使用xsd反序列化xml文件

---
+++ Samples/xsd/xsd/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace xsd
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string content = System.IO.File.ReadAllText("queue.xml");
+            eDSPNNet note = (eDSPNNet)SerializeHelp.Deserialize(typeof(eDSPNNet), content);
+        }
+    }
+}
---
+++ Samples/xsd/xsd/SerializeHelp.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace xsd
+{
+    public class SerializeHelp
+    {
+        public static string Serialize(System.Type type, object o)
+        {
+            string result = string.Empty;
+            try
+            {
+                XmlSerializer xs = new XmlSerializer(type);
+                MemoryStream ms = new MemoryStream();
+                xs.Serialize(ms, o);
+                ms.Seek(0, SeekOrigin.Begin);
+                StreamReader sr = new StreamReader(ms);
+                result = sr.ReadToEnd();
+                ms.Close();
+            }
+            catch (Exception ex)
+            {
+                // nLogInfo.Write(ex.Message + ex.StackTrace);
+            }
+            return result;
+        }
+
+        public static object Deserialize(System.Type type, string xml)
+        {
+            object root = new object();
+            try
+            {
+                XmlSerializer xs = new XmlSerializer(type);
+                MemoryStream ms = new MemoryStream();
+                StreamWriter sw = new StreamWriter(ms);
+                sw.Write(xml);
+                sw.Flush();
+                ms.Seek(0, SeekOrigin.Begin);
+                root = xs.Deserialize(ms);
+                ms.Close();
+            }
+            catch (Exception ex)
+            {
+                //   nLogInfo.Write(ex.Message + ex.StackTrace);
+            }
+            return root;
+        }
+    }
+}
